83e4a1df092420d032c942967fdf247de9187c64
SandaRuwan 3 <102467041+SadaruwanItzMe3@users.noreply.github.com>
2026-01-16 21:44:53 +0530

    Update register.js

--- register.js
+++ register.js
@@ -8,6 +8,6 @@ const rest = new REST({ version: "10" })
   .setToken(process.env.TOKEN);
 
 rest.put(
-  Routes.applicationCommands("1242072663250174053"),
+  Routes.applicationCommands("1461743846189695131"),
   { body: [command.toJSON()] }
 ).then(() => console.log("✅ Slash command registered"));
